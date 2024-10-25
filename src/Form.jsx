a9cbdbfb0b02137ee86ff8f2705724ea2677fca6
Durgesh Tiwari <twdurgesh226@gmail.com>
2024-10-26 02:48:46 +0530

    Your descriptive message for the second commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -6,11 +6,7 @@ const DiaryForm = () => {
   
   return (
     <div className="form-container">
-      <h1> I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like Neaxt.js, React.js, Node.js, and
-        Express.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!</h1>
+      <h1> I'm a skilled software developer with a solid foundation in TypeScript and JavaScript, along with extensive expertise in leading frameworks like Next.js, React.js, Node.js, and Express.js. My experience spans full-stack development, including database management with MongoDB, PostgreSQL, and Prisma, enabling me to build efficient, data-driven applications. As a quick learner, I continually stay updated on industry trends, integrating the latest technologies to ensure scalability, performance, and long-term stability in every project. I work collaboratively with clients, taking the time to understand their unique requirements and transforming ideas into practical, user-friendly solutions that address real-world challenges. If you’re looking to turn your vision into a powerful application, let’s work together to bring your ideas to life!</h1>
       
     </div>
   );
